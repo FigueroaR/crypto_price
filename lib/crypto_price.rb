8fad18c47a4c94f0c7675f0a3042deb580138764
Raul Figueroa <raul.a.figueroa@hotmail.com>
2019-09-19 22:13:10 +0000

    update

--- lib/crypto_price.rb
+++ lib/crypto_price.rb
@@ -3,9 +3,9 @@ require 'nokogiri'
 require 'pry'
 
 
-require_relative "crypto_price/version"
-require_relative "crypto_price/cli"
-require_relative "crypto_price/price"
+require_relative "./crypto_price/version"
+require_relative './crypto_price/cli'
+require_relative './crypto_price/price'
 
 
 module CryptoPrice
