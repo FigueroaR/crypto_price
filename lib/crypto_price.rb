4b12602933fe1645973b368cb3cc669da18c8606
Raul Figueroa <raul.a.figueroa@hotmail.com>
2019-09-21 18:37:00 +0000

    updated

--- lib/crypto_price.rb
+++ lib/crypto_price.rb
@@ -7,7 +7,3 @@ require_relative "./crypto_price/version"
 require_relative './crypto_price/price'
 require_relative './crypto_price/cli'
 
-module CryptoPrice
-  class Error < StandardError; end
-  # Your code goes here...
-end
