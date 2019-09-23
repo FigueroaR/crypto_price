0df2633da7aa3d610311ac6911f522ed0f136e34
figueroar <raul.a.figueroa@hotmail.com>
2019-09-23 16:25:56 -0400

    vsc update

--- lib/crypto_price/cli.rb
+++ lib/crypto_price/cli.rb
@@ -7,27 +7,25 @@ class CryptoPrice::CLI
     #goodbye 
   end 
   
-  
   def menu
     puts " Search through all Cryptocurrencies and find the information you want! "
     input = gets.strip
+    puts"choose adn option:"
     case input
+      
     when "1"
       "puts this an option"
     end 
+    #binding.pry
   end 
   
   #def current_price 
     #puts "Cryptocurrenrcy prices Today"
     #puts " "
-   
-        
+
       #@prices = CryptoPrice::Price.now
       #@prices.each.with_index(1) do |currency, i|
           #puts "#{i}. #{currency.name} - #{currency.price} - #{currency.change} - #{currency.volume} - #{currency.circulatingSupply} -     #{currency.marketcap} "
      
   #end 
-  
-  
-  
 end 
