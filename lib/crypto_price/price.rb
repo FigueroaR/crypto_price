420f82249ef92f278291bf8821dde4bf5896560a
figueroar <raul.a.figueroa@hotmail.com>
2019-09-26 21:59:16 -0400

    scrape updates vsc

--- lib/crypto_price/price.rb
+++ lib/crypto_price/price.rb
@@ -1,6 +1,6 @@
 class CryptoPrice::Price
   
-  attr_accessor :symbol, :name, :price,:change, :marketcap, :volume, :circulatingSupply
+  attr_accessor :index, :name, :price, :changeSign, :percent
   
   def self.now 
     self.scrape_array
@@ -15,18 +15,15 @@ class CryptoPrice::Price
   
   def self.scrape_coinmarketcap
     doc = Nokogiri::HTML(open("https://www.coinbase.com/price"))
-    #puts " gets scraped from info here"
     #binding.pry 
     coin = self.new  
-    #coin.name = doc.css("td.no-wrap").text.strip
-    #coin.symbol = doc.css("span.currency-symbol").text #old website
+    #coin.symbol = doc.css("tbody.AssetTable__AssetTableBody-sc-3hlimn-0.bpsIBu div").first.css("h4.Header__StyledHeader-sc-1q6y56a-0").text
     coin.name = doc.css("tbody.AssetTable__AssetTableBody-sc-3hlimn-0.bpsIBu div").first.css("h4.Header__StyledHeader-sc-1q6y56a-0").text
+    coin.index = doc.css("div.Flex-sc-12n1bmd-0 td").first.css("h4.Header__StyledHeader-sc-1q6y56a-0").text
+    coin.changeSign = doc.css("div.PercentChange__Change-sc-1ukqm0f-0").first.css("span.PercentChange___Symbol-sc-1ukqm0f-1.fHPgbe").text 
+    #coin.percent = doc.css("div.Flex-sc-12n1bmd-0 td").first.css("div.PercentChange__Change-sc-1ukqm0f-0.jDVmZV").text 
+    coin.price = doc.css("h4.Header__StyledHeader-sc-1q6y56a-0.hZxUBM").text  #first.css("h4.Header__StyledHeader-sc-1q6y56a-0.hZxUBM.TextElement__Spacer-sc-18l8wi5-0.hpeTzd").text 
     binding.pry
-    #coin.price = 
-    #coin.chagen = 
-    #coin.volume = 
-    #coin.marketcap = 
-    #coin.circulatingSupply = 
-    #coin
+    coin
   end 
 end 
